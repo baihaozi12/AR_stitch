a8ad3975446976b8e2d89ad30126aab28327ba10
Grapevine <1792127108@qq.com>
2020-07-09 13:38:59 +0800

    change point calculation

--- main.cpp
+++ main.cpp
@@ -11,21 +11,21 @@ using namespace std;
 
 int main()
 {
-    Mat im0 = imread("/home/baihao/jpg/1111111/1.jpg", cv::COLOR_BGR2GRAY);
-    Mat im1 = imread("/home/baihao/jpg/1111111/2.jpg", cv::COLOR_BGR2GRAY);
+    Mat im0 = imread("/home/baihao/jpg/1111111/verti3/3_1.jpg", cv::COLOR_BGR2GRAY);
+    Mat im1 = imread("/home/baihao/jpg/1111111/verti3/3_2.jpg", cv::COLOR_BGR2GRAY);
 //    Eigen::Matrix<>
     if(! im0.data) {
         cout << "read image error" << endl;
     }
     featuredata *basedata = new featuredata();
 
-    getfeaturedata(*basedata, im0, 1, 0.8, 0.5);
+    getfeaturedata(*basedata, im0, 3, 0.8, 0.5);
 
     clock_t start, finish;
     start = clock();
 
     stitch_status *result = new stitch_status();
-    check_image_v2(*result, *basedata, im1, 0, 0.8, 0.5, 10, 20, 3, 1);
+    check_image_v2(*result, *basedata, im1, 2, 0.8, 0.5, 10, 20, 3, 1);
 
 
     cout << result->direction_status << endl;
